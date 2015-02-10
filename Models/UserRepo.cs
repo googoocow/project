547865cd763ab35a65e0ba16a6649f29f0be27a6
googoocow <dfsandlrr@gmail.com>
2015-02-10 10:24:15 -0800

    MasterBlaster

---
+++ Models/UserRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhaseTwo.Models
+{
+    public class UserRepo
+    {
+        static PhaseTwoDBEntities ctx = new PhaseTwoDBEntities();
+
+        public static bool UserNameExists(string username)
+        {
+            return (ctx.Users.Where(a => a.username == username).Count() == 1);
+        }
+    }
+}
